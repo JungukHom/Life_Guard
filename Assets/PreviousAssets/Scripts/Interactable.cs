498164dd08e3e13984c006469abab5bd2948cfd2
JungukHom <developer0223@gmail.com>
2021-02-16 17:12:44 +0900

    Add | Previous lifeguard assets

---
+++ Assets/PreviousAssets/Scripts/Interactable.cs
@@ -0,0 +1,34 @@
+﻿namespace RESC
+{
+    // C#
+    using System.Collections;
+    using System.Collections.Generic;
+
+    // Unity
+    using UnityEngine;
+
+    // Project
+    // Alias
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class Interactable : MonoBehaviour
+    {
+        public Vector3 m_Offset = Vector3.zero;
+
+        [HideInInspector]
+        public Hand m_ActiveHand = null;
+
+        public virtual void Action()
+        {
+            print("Action");
+        }
+
+        public void ApplyOffset(Transform hand)
+        {
+            transform.SetParent(hand);
+            transform.localRotation = Quaternion.identity;
+            transform.localPosition = m_Offset;
+            transform.SetParent(null);
+        }
+    }
+}
